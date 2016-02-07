8124a97ccdccd3ea3b90a2b8ee7698cd3c41d587
Luciano Ratamero <lucianoratamero@gmail.com>
2016-02-07 03:40:56 -0200

    Adds api to respond lotto ticket results (with random winner).

--- bob/views.py
+++ bob/views.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+import random
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from bob.serializers import LottoTicketSerializer
+
 
 class LottoTicketResultAPIView(APIView):
 
     def post(self, request, format='json'):
-        return Response(request.data, status=status.HTTP_201_CREATED)
+
+        serializer = LottoTicketSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(is_winner=bool(random.getrandbits(1)))
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
