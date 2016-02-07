038eb9abc8177b7733301afffe3fea9e107a05fd
Luciano Ratamero <lucianoratamero@gmail.com>
2016-02-06 22:37:17 -0200

    Adds Ana and Bob’ LottoTicket models.

--- ana/models.py
+++ ana/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from core.models import LottoTicket
 
-# Create your models here.
+
+class LottoTicket(LottoTicket):
+    app_label = 'ana'
