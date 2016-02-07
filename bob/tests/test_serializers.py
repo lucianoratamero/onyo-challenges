f4e8df06cea24e9d38ddf5251b367d3969a62530
Luciano Ratamero <lucianoratamero@gmail.com>
2016-02-07 03:09:19 -0200

    Adds LottoTicket serializer.

---
+++ bob/tests/test_serializers.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+from bob.serializers import LottoTicketSerializer
+
+
+class LottoTicketSerializerTestCase(TestCase):
+
+    def test_serializer_required_fields(self):
+        serializer = LottoTicketSerializer(data={})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('numbers', serializer.errors)
