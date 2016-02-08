b1601e8d57d011e47f83288f7b33cc7fd2b0f03e
Luciano Ratamero <lucianoratamero@gmail.com>
2016-02-07 22:59:08 -0200

    Includes ana’s and bob’s models to admin.

--- bob/admin.py
+++ bob/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from bob.models import LottoTicket
+
+
+class LottoTicketAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(LottoTicket, LottoTicketAdmin)
