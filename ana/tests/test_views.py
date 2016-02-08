255ce3e4ba75954851920689b0f9efe18fff8254
Luciano Ratamero <lucianoratamero@gmail.com>
2016-02-07 22:58:39 -0200

    Adds API view to retrieve lotto ticket results to ana.

--- ana/tests/test_views.py
+++ ana/tests/test_views.py
@@ -2,34 +2,42 @@
 from mock import patch
 
 from django.core.urlresolvers import reverse
+from django.test import LiveServerTestCase
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from ana.models import LottoTicket
+from ana.exceptions import BobTicketBadRequestException
 
 
-class LottoTicketResultAPIViewTestCase(APITestCase):
+class LottoTicketResultAPIViewTestCase(APITestCase, LiveServerTestCase):
 
     def setUp(self):
-        LottoTicket.objects.all().delete()
         self.url = reverse('ana:lotto_ticket_result')
         self.data = {'numbers': [1, 2, 3, 4, 5, 6]}
+
+    def tearDown(self):
+        LottoTicket.objects.all().delete()
 
     # Unit Tests
 
     @patch('ana.views.LottoTicketSerializer')
-    def test_unit__post_returns_201_CREATED_if_serializer_is_valid(self, mocked_serializer):
-        expected_data = self.data
-        expected_data.update({'is_winner': False})
+    @patch('ana.views.LottoTicketRepository')
+    def test_unit__post_returns_200_OK_if_serializer_is_valid(self, mocked_repository, mocked_serializer):
+        dummy_lotto_ticket = LottoTicket(is_winner=False, numbers=[1, 2, 3, 4, 5, 6])
+        dummy_lotto_ticket.save()
+
         mocked_serializer().is_valid.return_value = True
-        mocked_serializer().data = expected_data
+        mocked_repository().get_by_numbers.return_value = dummy_lotto_ticket
 
         response = self.client.post(self.url, self.data, format='json')
 
-        self.assertTrue(mocked_serializer().save.called)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.content, '{"is_winner":false,"numbers":[1,2,3,4,5,6]}')
+        self.assertTrue(mocked_repository().get_by_numbers.called)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('id', response.content)
+        self.assertIn('is_winner', response.content)
+        self.assertIn('numbers', response.content)
 
     @patch('ana.views.LottoTicketSerializer')
     def test_unit__post_returns_400_BAD_REQUEST_if_serializer_is_not_valid(self, mocked_serializer):
@@ -41,14 +49,33 @@ class LottoTicketResultAPIViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.content, '{"numbers":["This field is required."]}')
 
+    @patch('ana.views.LottoTicketSerializer')
+    @patch('ana.views.LottoTicketRepository')
+    def test_unit__post_returns_400_BAD_REQUEST_if_repository_raises_bad_request_exception(self, mocked_repository, mocked_serializer):
+        mocked_serializer().is_valid.return_value = True
+        mocked_repository().get_by_numbers.side_effect = BobTicketBadRequestException
+
+        response = self.client.post(self.url, self.data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.content, '{"reason":"Retrieving data from bob has failed."}')
+
     # Integration Tests
 
-    def test_integration__post_returns_201_CREATED_if_serializer_is_valid(self):
+    def test_integration__post_returns_200_OK_if_serializer_is_valid(self):
         self.assertEqual(0, LottoTicket.objects.count())
 
         response = self.client.post(self.url, self.data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('id', response.content)
+        self.assertIn('is_winner', response.content)
+        self.assertIn('numbers', response.content)
+        self.assertEqual(1, LottoTicket.objects.count())
+
+        response = self.client.post(self.url, self.data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('id', response.content)
         self.assertIn('is_winner', response.content)
         self.assertIn('numbers', response.content)
--- config/dev/settings.py
+++ config/dev/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from config.base_settings import *
 from config.ana.settings import *
 from config.bob.settings import *
@@ -6,3 +8,9 @@ DEBUG = True
 
 CURRENT_BOB_API_URL = 'http://localhost:8000/api/v0/bob/'
 
+'''
+Configuration needed to enable communication between APIs
+in services during tests
+'''
+
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8000'
